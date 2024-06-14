2d8e386afe26bf830a5e57f10d236a5066eca30f
HARISHKUMAR-IT <231001060@rajalakshmi.edu.in>
2024-06-14 18:46:20 +0530

    Create 4.1.py

---
+++ WEEK-4/4.1.py
@@ -0,0 +1,45 @@
+Given an integer N, check whether N the given number can be made a perfect square after adding 1 to it.
+
+Input Format:
+
+Single integer input.
+
+Output Format:
+
+Yes or No.
+
+Example Input:
+
+24
+
+Output:
+
+Yes
+
+Example Input:
+
+26
+
+Output:
+
+No
+
+For example:
+
+Input Result
+
+24    Yes
+
+Code:
+
+n=int(input())
+a=n+1 
+c=0 
+while True:
+  if c*c==a: 
+    print("Yes") 
+    break 
+  if (c*c)>a: 
+    print("No")
+    break
+  c=c+1
