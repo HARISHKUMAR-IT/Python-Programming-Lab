1d7f84dda7598bc7d80c9d2eb04e818e2dbf7e08
HARISHKUMAR-IT <231001060@rajalakshmi.edu.in>
2024-06-15 14:33:38 +0530

    Create 9.1.py

---
+++ WEEK-9/9.1.py
@@ -0,0 +1,22 @@
+An automorphic number is a number whose square ends with the number itself.
+
+For example, 5 is an automorphic number because 5*5 =25. The last digit is 5 which same
+
+as the given number.
+
+If the number is not valid, it should display “Invalid input”.
+
+If it is an automorphic number display “Automorphic” else display “Not Automorphic”.
+
+Input Format:
+
+Take a Integer from Stdin Output Format: Print Automorphic if given number is Automorphic number,otherwise Not Automorphic Example input: 5 Output: Automorphic Example input: 25 Output: Automorphic Example input: 7 Output: Not Automorphic
+
+Code:
+def automorphic(n):
+    s=n*n
+    b=len(str(s))
+    if s%(10**(b-1))==n:
+        return "Automorphic"
+    return "Not Automorphic"    
+    
