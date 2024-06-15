43123e71dc51689e8f8f0b36c7a310f1f17d681d
HARISHKUMAR-IT <231001060@rajalakshmi.edu.in>
2024-06-15 14:59:01 +0530

    Create Ex.2.py

---
+++ WEEK-3 EXTRA/Ex.2.py
@@ -0,0 +1,27 @@
+2. Write a program that reads an integer from the user. Then your program should display a message indicating whether the integer is even or odd.
+Sample Input 1
+5
+Sample Output 1
+5 is odd.
+Sample Input 2
+10
+Sample Output 2
+10 is even.
+For example:
+Input
+Result
+5
+5 is odd.
+
+
+
+#Even or odd
+n=int(input())
+if(n%2==0):
+    print(n,"is even.")
+else:
+    print(n,"is odd.")
+
+
+
+
