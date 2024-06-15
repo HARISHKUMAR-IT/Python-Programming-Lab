4532c1adf7a483da011eaad7a479ea2bc8fdf324
HARISHKUMAR-IT <231001060@rajalakshmi.edu.in>
2024-06-15 14:04:27 +0530

    Create 5.4.py

---
+++ WEEK-5/5.4.py
@@ -0,0 +1,36 @@
+Given two Strings s1 and s2, remove all the characters from s1 which is present in s2.
+
+ 
+
+Constraints
+
+ 
+
+1<= string length <= 200
+
+ 
+
+Sample Input 1
+
+ 
+
+experience
+
+enc
+
+ 
+
+Sample Output 1
+
+ 
+
+xpri
+
+Program:
+s1=input()
+s2=input()
+s=''
+for i in range(0,len(s1)):
+    if s1[i] not in s2:
+        s=s+s1[i]
+print(s)
