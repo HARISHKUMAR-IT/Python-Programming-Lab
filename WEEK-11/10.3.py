347f7f4286b6bf7269e65405ca92a35e54af0b4b
HARISHKUMAR-IT <231001060@rajalakshmi.edu.in>
2024-06-15 14:48:36 +0530

    Create 10.3.py

---
+++ WEEK-11/10.3.py
@@ -0,0 +1,36 @@
+Problem Description:
+
+Write a Python program that asks the user for their age and prints a message based on the age. Ensure that the program handles cases where the input is not a valid integer.
+
+
+
+Input Format:
+
+A single line input representing the user's age.
+
+
+
+Output Format:
+
+Print a message based on the age or an error if the input is invalid.
+
+For example:
+
+Input	Result
+25
+You are 25 years old.
+rec
+Error: Please enter a valid age.
+-5
+Error: Please enter a valid age
+
+Code:
+#Age exception
+try:
+    n=int(input())
+    if n>=0:
+        print("You are %d years old."%n)
+    else:
+        raise Exception
+except:
+    print("Error: Please enter a valid age.")
