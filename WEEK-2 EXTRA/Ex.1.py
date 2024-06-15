73efa48bcc2b5f93f7a1c311d16d89700655d3ed
HARISHKUMAR-IT <231001060@rajalakshmi.edu.in>
2024-06-15 14:56:25 +0530

    Create Ex.1.py

---
+++ WEEK-2 EXTRA/Ex.1.py
@@ -0,0 +1,18 @@
+1.Given an integer n, print true if it is a power of three. Otherwise, print false.
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+#power of three
+import math
+n=int(input())
+
+i=0
+
+while True:
+    if 3**i==n:
+        print("True")
+        break
+    if 3**i>n:
+        print("False")
+        break
+    i=i+1
+
